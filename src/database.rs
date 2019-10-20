1891ab4835fe1ec7aabdb1d34c78952ed925e6d1
Steven Joruk <steven@joruk.com>
2019-10-20 16:27:45 +0100

    Load the database from a resource if it isn't on disk

--- src/database.rs
+++ src/database.rs
@@ -6,15 +6,33 @@ use crate::addon::Addon;
 use crate::error::Result;
 use serde_json;
 
-pub struct Database;
+const DEFAULT_DATA: &str = include_str!("../res/wow-classic.json");
+
+pub struct Database {
+    pub addons: HashSet<Addon>,
+}
 
 impl Database {
-    pub fn load<P: AsRef<Path>>(path: P) -> Result<HashSet<Addon>> {
-        println!("Opening {}", path.as_ref().display());
-        let file = fs::File::open(path)?;
-        let reader = io::BufReader::new(file);
-        let addons: HashSet<Addon> = serde_json::from_reader(reader)?;
+    pub fn update(&self) {
+        println!("TODO: Update");
+    }
 
-        Ok(addons)
+    pub fn load<P: AsRef<Path>>(path: P) -> Result<Database> {
+        let addons: HashSet<Addon>;
+
+        match fs::File::open(path) {
+            Ok(file) => {
+                println!("Loading the database from {}", path.as_ref().display());
+                let reader = io::BufReader::new(file);
+                addons = serde_json::from_reader(reader)?;
+            }
+            Err(e) => {
+                eprintln!("Unable to open {}: {}", path.as_ref().display(), e);
+                println!("Loading the built in database");
+                addons = serde_json::from_str(DEFAULT_DATA)?;
+            }
+        };
+
+        Ok(Database { addons })
     }
 }
